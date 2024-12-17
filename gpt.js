dc048d82a1425916b6edb166c016620d381dd9d8
czbashl <jie87981@gmail.com>
2024-12-17 14:21:40 +0800

    Create gpt.js

---
+++ gpt.js
@@ -0,0 +1,38 @@
+{
+  "locked": 0,
+  "name": "gpt1",
+  "mitm": 0,
+  "paths": [
+    {
+      "schema": "https",
+      "host": "share.aivvm.org",
+      "pathMode": 0,
+      "query": "token=100150",
+      "rules": [
+        {
+          "regularExpression": 1,
+          "redirectCode": "code302",
+          "matchKey": "100150",
+          "tempFile": "",
+          "mockType": "local",
+          "modifyValue": "100644",
+          "localFile": "",
+          "headerAction": "add",
+          "type": "request",
+          "serverFile": "",
+          "regularAction": "modifyUrl",
+          "mode": "regular",
+          "mockFileType": 0
+        }
+      ],
+      "regrexPath": "",
+      "path": "\/",
+      "method": "ALL",
+      "port": ""
+    }
+  ],
+  "desc": "",
+  "enabled": true,
+  "created_at": 1734415089959,
+  "updated_at": 1734415642407.8979
+}
